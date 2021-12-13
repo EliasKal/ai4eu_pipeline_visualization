fe60430a4691f3293a73d33285bd80a06b38958f
Christoforos <papasterc@iti.gr>
2021-12-13 12:42:28 +0200

    finish databroker code

--- databroker/databroker.py
+++ databroker/databroker.py
@@ -3,6 +3,8 @@ import haversine as hs
 import pandas as pd
 import numpy as np
 import random
+import time
+from concurrent import futures
 import grpc
 import databroker_pb2_grpc
 import databroker_pb2
@@ -22,7 +24,6 @@ gps_data = gps_data.iloc[:,1:49]
 #get a random row as next value
 row = random.randint(0, no2_data.shape[0])
 
-
 #calculate and add distances to datasets
 for i in range(gps_data.shape[1]//2):
     lat2 = gps_data.iloc[0,i*2+0]
@@ -33,13 +34,6 @@ for i in range(gps_data.shape[1]//2):
     no2_data.iloc[0,1+i] = distance
     pm10_data.iloc[0,1+i] = distance
     pm25_data.iloc[0,1+i] = distance
-
-
-
-no2_input = np.ndarray.tobytes(no2_input)
-pm10_input = np.ndarray.tobytes(pm10_input)
-pm25_input = np.ndarray.tobytes(pm25_input)
-
 
 
 #databroker class
@@ -58,18 +52,34 @@ class Databroker(databroker_pb2_grpc.DatabrokerServicer):
             no2_input= no2_data.iloc[self.current_row,1:].to_numpy()
             pm10_input= pm10_data.iloc[self.current_row,1:].to_numpy()
             pm25_input= pm25_data.iloc[self.current_row,1:].to_numpy()
+
             #convert output to byte code(due to many columns per dataset)
             no2_input = np.ndarray.tobytes(no2_input)
             pm10_input = np.ndarray.tobytes(pm10_input)
             pm25_input = np.ndarray.tobytes(pm25_input)
 
-
+            #add output to response
+            response.no2_data = no2_input
+            response.pm10_data = pm10_input
+            response.pm25_data = pm25_input
+            
+            #add 1 to row counter(maybe we could make it cyclical with mod later)
             self.current_row += 1
 
         return response
 
 
+#host server
+server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+databroker_pb2_grpc.add_DatabrokerServicer_to_server(Databroker(), server)
+print("Starting server. Listening on port : " + str(port))
+server.add_insecure_port("[::]:{}".format(port))
+server.start()
 
-        return no2_input, pm10_input, pm25_input
+try:
+    while True:
+        time.sleep(86400)
+except KeyboardInterrupt:
+    server.stop(0)
 
 
